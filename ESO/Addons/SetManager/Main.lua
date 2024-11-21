81d563f1d76ca23b8b8971ccd826efa470fd14d5
samanthasmedley174 <samanthasmedley174@gmail.com>
2024-11-21 19:59:31 +0000

    updated code

--- ESO/Addons/SetManager/Main.lua
+++ ESO/Addons/SetManager/Main.lua
@@ -2,149 +2,51 @@ local addon = {
 	name = "SetManager",
 	playerDefaults =
 	{
-		sets = { },
-		worn = { },
-		mode = "INVENTORY",
-		quality = ITEM_QUALITY_MAGIC,
+		sets = { }
 	},
 	accountDefaults =
 	{
-		sets = { },
-		templates = { },
-	},
-	setCategory =
-	{
-		NonCraftable = "NON_CRAFTABLE",
-		Craftable = "CRAFTABLE",
-	},
-	SetType =
-	{
-		Craftable = "CRAFTABLE",
-		Monster = "MONSTER",
-		Jevelry = "JEVELRY",
+		sets = { }
 	},
 }
 
--- local am = GetAnimationManager()
+local am = GetAnimationManager()
 local wm = GetWindowManager()
 local em = GetEventManager()
 
-do
-	local function OnSlotClicked(parent, control, ...)
-		for equipSlot, other in pairs(parent.slots) do
-			local selected = other == control
-			if other:GetState() ~= BSTATE_DISABLED then
-				other:SetState(selected and BSTATE_PRESSED or BSTATE_NORMAL)
-			end
-			if selected then parent.selectedSlot = equipSlot end
-		end
-		if parent.OnSelectedChanged then parent.OnSelectedChanged(parent, ...) end
-	end
-
-	local function UpdateSlot(self)
-		local iconControl = self:GetNamedChild("Icon")
-		if self.itemLink then
-			local icon = GetItemLinkInfo(self.itemLink)
-			iconControl:SetTexture(icon)
-		else
-			iconControl:SetTexture(ZO_Character_GetEmptyEquipSlotTexture(self.slotId))
-		end
-	end
-
-	-- local function ReturnItemLink(itemLink) d(itemLink) return itemLink end
-	-- local function OnMouseDown(self, button)
-	-- 	if button == MOUSE_BUTTON_INDEX_RIGHT then
-	-- 		if IsChatSystemAvailableForCurrentPlatform() and self.data then
-	-- 			ClearMenu()
-	-- 			AddCustomMenuItem(GetString(SI_ITEM_ACTION_LINK_TO_CHAT), function()
-	-- 				local selectedSet = self.data
-	-- 				if not selectedSet then return end
-	-- 				local link = addon.ChatLinkHandler:CreateChatLink(selectedSet)
-	-- 				d(link)
-	-- 				ZO_LinkHandler_InsertLink(ZO_LinkHandler_CreateChatLink(ReturnItemLink, link))
-	-- 			end )
-	-- 			ShowMenu(control)
-	-- 		end
-	-- 	end
-	-- end
-
-	function addon:InitializeSlots(parent)
-		if parent.slots then return end
-
-		local slots =
-		{
-			[EQUIP_SLOT_HEAD] = parent:GetNamedChild("EquipmentSlotsHead"),
-			[EQUIP_SLOT_NECK] = parent:GetNamedChild("EquipmentSlotsNeck"),
-			[EQUIP_SLOT_CHEST] = parent:GetNamedChild("EquipmentSlotsChest"),
-			[EQUIP_SLOT_SHOULDERS] = parent:GetNamedChild("EquipmentSlotsShoulder"),
-			[EQUIP_SLOT_MAIN_HAND] = parent:GetNamedChild("EquipmentSlotsMainHand"),
-			[EQUIP_SLOT_OFF_HAND] = parent:GetNamedChild("EquipmentSlotsOffHand"),
-			[EQUIP_SLOT_POISON] = parent:GetNamedChild("EquipmentSlotsPoison"),
-			[EQUIP_SLOT_WAIST] = parent:GetNamedChild("EquipmentSlotsBelt"),
-			[EQUIP_SLOT_LEGS] = parent:GetNamedChild("EquipmentSlotsLeg"),
-			[EQUIP_SLOT_FEET] = parent:GetNamedChild("EquipmentSlotsFoot"),
-			[EQUIP_SLOT_COSTUME] = parent:GetNamedChild("EquipmentSlotsCostume"),
-			[EQUIP_SLOT_RING1] = parent:GetNamedChild("EquipmentSlotsRing1"),
-			[EQUIP_SLOT_RING2] = parent:GetNamedChild("EquipmentSlotsRing2"),
-			[EQUIP_SLOT_HAND] = parent:GetNamedChild("EquipmentSlotsGlove"),
-			[EQUIP_SLOT_BACKUP_MAIN] = parent:GetNamedChild("EquipmentSlotsBackupMain"),
-			[EQUIP_SLOT_BACKUP_OFF] = parent:GetNamedChild("EquipmentSlotsBackupOff"),
-			[EQUIP_SLOT_BACKUP_POISON] = parent:GetNamedChild("EquipmentSlotsBackupPoison"),
-		}
-
-		parent.slots = slots
-
-		parent:GetNamedChild("PaperDoll"):SetTexture(GetUnitSilhouetteTexture("player"))
-
-		parent.OnSlotClicked = OnSlotClicked
-		local ZO_Character_GetEmptyEquipSlotTexture = ZO_Character_GetEmptyEquipSlotTexture
-		for slotId, slotControl in pairs(slots) do
-			slotControl.slotId = slotId
-			slotControl.Update = UpdateSlot
-			slotControl:SetHandler("OnClicked", function(control, ...) control:GetParent():OnSlotClicked(control, ...) end)
-		end
-		-- parent:SetHandler("OnMouseDown", OnMouseDown)
-	end
-end
-
-function addon:Init()
-	SLASH_COMMANDS["/setm"] = function(...) addon:cmdSetManager(...) end
-
-	local LMM2 = LibStub("LibMainMenu-2.0")
-	LMM2:Init()
-	self.LMM2 = LMM2
-
-	self.Designer:Init()
-	self.Selector:Init()
-
-	self:InitInventoryScan()
-end
-
-local function PlayerActivated()
+local function OnUnitCreated()
 	em:UnregisterForEvent(addon.name, EVENT_PLAYER_ACTIVATED)
-	-- reserved
+	SetManager_CharacterPaperDoll:SetTexture(GetUnitSilhouetteTexture("player"))
+	-- RefreshWornInventory()
+	-- RefreshBackUpWeaponSlotStates()
+end
+
+local function PlayerDeactivated()
+end
+
+function addon:InitWindow()
+	local control
+	control = CreateControlFromVirtual("SetManager_Character", nil, "SetManager_Character_Template")
+	control:SetAnchor(TOPLEFT, ZO_SharedWideLeftPanelBackground, TOPLEFT, 0, -40)
+	control:SetAnchor(BOTTOMLEFT, ZO_SharedWideLeftPanelBackground, BOTTOMLEFT, 0, -30)
+
+	addon.windowSet = control
+	SETMANAGER_CHARACTER_FRAGMENT = ZO_FadeSceneFragment:New(addon.windowSet, false, 0)
+	STATS_SCENE:AddFragment(THIN_LEFT_PANEL_BG_FRAGMENT)
+	STATS_SCENE:AddFragment(SETMANAGER_CHARACTER_FRAGMENT)
+
+	em:RegisterForEvent(addon.name, EVENT_PLAYER_ACTIVATED, OnUnitCreated)
+	em:RegisterForEvent(addon.name, EVENT_PLAYER_DEACTIVATED, PlayerDeactivated)
 end
 
 local function OnAddonLoaded(event, name)
 	if name ~= addon.name then return end
 	em:UnregisterForEvent(addon.name, EVENT_ADD_ON_LOADED)
 
-	addon.player = ZO_SavedVars:NewCharacterIdSettings("SetManager_Data", 1, nil, addon.playerDefaults, nil)
+	addon.player = ZO_SavedVars:New("SetManager_Data", 1, nil, addon.playerDefaults, nil)
 	addon.account = ZO_SavedVars:NewAccountWide("SetManager_Data", 1, nil, addon.accountDefaults, nil)
-	addon.account.all = nil
 
-	local templates = addon.account.templates
-	if #templates == 0 then
-		templates[#templates + 1] = { }
-	end
-
-	addon:Init()
-
-	em:RegisterForEvent(addon.name, EVENT_PLAYER_ACTIVATED, PlayerActivated)
-end
-
-function addon:ToggleEditorScene()
-	self.LMM2:SelectMenuItem(self.name)
+	addon:InitWindow()
 end
 
 em:RegisterForEvent(addon.name, EVENT_ADD_ON_LOADED, OnAddonLoaded)
