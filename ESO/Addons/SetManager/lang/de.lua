997417d56064f8c1a1e328dcc8e3755f8087a1ab
samanthasmedley174 <samanthasmedley174@gmail.com>
2024-11-21 20:01:15 +0000

    updated code

--- ESO/Addons/SetManager/lang/de.lua
+++ ESO/Addons/SetManager/lang/de.lua
@@ -1,5 +1,5 @@
---SafeAddString(SI_BINDING_NAME_SET_MANAGER, "Set Manager", 0)
---SafeAddString(SI_KEYBINDINGS_LAYER_SET_MANAGER, "Set Manager", 0)
+SafeAddString(SI_BINDING_NAME_SET_MANAGER, "Set Manager", 0)
+SafeAddString(SI_KEYBINDINGS_LAYER_SET_MANAGER, "Set Manager", 0)
 
 SafeAddString(SI_BINDING_NAME_SET_MANAGER_ADD_SET, "Neues Set", 0)
 SafeAddString(SI_BINDING_NAME_SET_MANAGER_DElETE_SET, "Set löschen", 0)
@@ -7,5 +7,3 @@ SafeAddString(SI_BINDING_NAME_SET_MANAGER_COPY_EQUIPPED, "Ausrüstung übernehme
 
 SafeAddString(SET_MANAGER_APPLY_ITEM, "Hinzufügen", 0)
 SafeAddString(SET_MANAGER_REMOVE_ITEM, "Entfernen", 0)
-
-SafeAddString(SI_BINDING_NAME_SET_MANAGER_WRIT_POPUP, "In Popup anzeigen", 0)
